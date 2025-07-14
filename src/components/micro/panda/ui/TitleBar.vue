bbdd29ea81989435c4d379dcea05f209f49766e3
d.rogozhnikov <d.rogozhnikov@21vek.by>
2025-07-14 16:53:23 +0300

    Updated: titlebar

--- src/components/micro/panda/ui/TitleBar.vue
+++ src/components/micro/panda/ui/TitleBar.vue
@@ -1,17 +1,42 @@
 <template>
   <div class="sections-row">
-    <v-btn
+    <div class="section add-btn">
+      <v-btn
         icon="mdi-plus"
         @click="showAddForm"
-    />
-    <v-text-field clearable
-                  label="Search"
-                  variant="outlined"
-                  density="compact"
-                  append-inner-icon="mdi-magnify"
-                  hide-details
-                  :maxlength="10"
-    />
+      />
+    </div>
+    <div class="section search-field">
+      <v-text-field clearable
+                    label="Search"
+                    variant="outlined"
+                    density="compact"
+                    append-inner-icon="mdi-magnify"
+                    hide-details
+                    :maxlength="10"
+      />
+    </div>
+    <div class="section add-btn">
+      <v-speed-dial
+        location="bottom center"
+        transition="scale-transition"
+      >
+        <template v-slot:activator="{ props: activatorProps }">
+          <v-fab
+            v-bind="activatorProps"
+            size="large"
+            icon="$vuetify"
+          ></v-fab>
+        </template>
+
+        <v-btn key="1" icon="$success"></v-btn>
+        <v-btn key="2" icon="$info"></v-btn>
+        <v-btn key="3" icon="$warning"></v-btn>
+        <v-btn key="4" icon="$error"></v-btn>
+      </v-speed-dial>
+    </div>
+
+
   </div>
 </template>
 
@@ -25,5 +50,15 @@ export default {
 </script>
 
 <style scoped lang="sass">
+.v-speed-dial
+  width: 70px
+
+.add-btn
+  margin-left: 15px
+  margin-right: 15px
+  width: 50px
+
+.search-field
+  width: 80%
 
 </style>
